81095992feb230d3bb2a239a726643af13997d6a
Ricardo <ricardo.machado.nwi@gmail.com>
2021-07-22 18:54:15 -0300

    Modulo completo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,24 @@
 import { SignInButton } from '../SignInButton'
+import { ActiveLink } from '../../components/ActiveLink';
+
 import styles from './styles.module.scss';
 
+
 export function Header() {
+    
+
     return(
-/*         <header className={styles.headerContainer}>
-            <div className={styles.headerContent}> */
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <img src='/images/logo.svg' alt='' />
+                {/*eslint-disable-next-line @next/next/no-img-element*/}
+                <img src="/images/logo.svg" alt='Ignews' />
                 <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
+                    <ActiveLink activeClassName={styles.active} href='/'>
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href='/posts' prefetch>
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
                 <SignInButton />
             </div>
