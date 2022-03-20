6259f6cb786c233c39655e40c8132e6b6f3b4e80
Ricardo <ricardo.machado.nwi@gmail.com>
2022-03-20 00:55:02 -0300

    Starting Tests

--- src/components/ActiveLink/index.tsx
+++ src/components/ActiveLink/index.tsx
@@ -1,22 +1,26 @@
-import { ReactElement , cloneElement} from 'react'
+import { ReactElement, cloneElement } from 'react'
 import Link, { LinkProps } from 'next/link'
-import { useRouter } from 'next/dist/client/router';
+import { useRouter } from 'next/dist/client/router'
 
-interface ActiveLinkProps extends LinkProps{
-    children : ReactElement
-    activeClassName : string
+interface ActiveLinkProps extends LinkProps {
+  children: ReactElement
+  activeClassName: string
 }
 
-export function ActiveLink( {children , activeClassName, ...rest} : ActiveLinkProps)  {
-    const { asPath } = useRouter()
+export function ActiveLink({
+  children,
+  activeClassName,
+  ...rest
+}: ActiveLinkProps) {
+  const { asPath } = useRouter()
 
-    const className = asPath === rest.href ? activeClassName : '';
+  const className = asPath === rest.href ? activeClassName : ''
 
-    return(
-        <Link {...rest}>
-            {cloneElement( children , {
-                className
-            } )}
-        </Link>
-    )
+  return (
+    <Link {...rest}>
+      {cloneElement(children, {
+        className,
+      })}
+    </Link>
+  )
 }
